864389751c735cad59a458ff88ea66b2bf2d2fd9
Morgan Ivey <morganivey@Morgans-MacBook-Pro.local>
2019-11-13 20:50:26 -0800

    Filter queries to return the latest departments of each employee

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -15,21 +15,25 @@ from employees
 where extract(year from hire_date) = 1986;
 
 -- 3. List the manager of each department with the following information:
---department number, department name, the manager's employee number, 
---last name, first name, and start and end employment dates.
-select concat(employees.first_name, ' ', employees.last_name) as Manager_Name, dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name,dept_manager.from_date, dept_manager.to_date
-from dept_manager inner join departments on dept_manager.dept_no= departments.dept_no
-inner join employees on dept_manager.emp_no = employees.emp_no;
+-- department number, department name, the manager's employee number, 
+-- last name, first name, and start and end employment dates.
+select distinct on (dept_manager.dept_no) dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name,dept_manager.from_date, dept_manager.to_date
+from dept_manager 
+inner join departments 
+on dept_manager.dept_no= departments.dept_no
+inner join employees 
+on dept_manager.emp_no = employees.emp_no
+order by dept_manager.dept_no, dept_manager.to_date DESC;
 
 -- 4. List the department of each employee with the following information: 
 --employee number, last name, first name, and department name.
-select e.emp_no, e.last_name, e.first_name,d.dept_name
+select distinct on (e.emp_no) e.emp_no, e.last_name, e.first_name,d.dept_name
 from employees as e
-inner join dept_emp as de
+left join dept_emp as de
 on e.emp_no = de.emp_no
 inner join departments as d
 on de.dept_no = d.dept_no
-order by e.emp_no;
+order by e.emp_no, de.to_date DESC;
 
 
 
@@ -40,26 +44,32 @@ from employees as e
 where (e.first_name = 'Hercules') and (lower(e.last_name) like 'b%')
 order by e.last_name;
 
+
+-- Create table with most current departments for each employee to use for #6 and #7 below:
+select distinct on (emp_no) *
+into current_dept_emp
+from dept_emp
+order by emp_no, to_date DESC;
+
 -- 6.List all employees in the Sales department, including their employee number, 
 --last name, first name, and department name.
 select e.emp_no, e.last_name, e.first_name, d.dept_name
 from employees as e
-inner join dept_emp as de
-on e.emp_no = de.emp_no
+inner join current_dept_emp as cde
+on e.emp_no = cde.emp_no
 inner join departments as d
-on de.dept_no = d.dept_no
+on cde.dept_no = d.dept_no
 where lower(d.dept_name) = 'sales';
 
 -- 7. List all employees in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name.
 select e.emp_no, e.last_name, e.first_name, d.dept_name
 from employees as e
-inner join dept_emp as de
-on e.emp_no = de.emp_no
+inner join current_dept_emp as cde
+on e.emp_no = cde.emp_no
 inner join departments as d
-on de.dept_no = d.dept_no
-where (lower(d.dept_name) = 'sales') or (lower(d.dept_name) = 'development')
-order by emp_no;
+on cde.dept_no = d.dept_no
+where (lower(d.dept_name) = 'sales') or (lower(d.dept_name) = 'development');
 
 -- 8. In descending order, list the frequency count of employee last names, 
 -- i.e., how many employees share each last name.
